8788bee9dc699ee8b0ce7ab0686dbcc0330712be
Julian Bright <brightsparc@gmail.com>
2023-01-31 20:44:47 +1100

    Upgrade EKS cluster to 1.24

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ module "eks_blueprints" {
   source  = "github.com/aws-ia/terraform-aws-eks-blueprints"
 
   cluster_name    = local.name
-  cluster_version = "1.23"
+  cluster_version = "1.24"
 
   vpc_id             = module.vpc.vpc_id
   private_subnet_ids = module.vpc.private_subnets
